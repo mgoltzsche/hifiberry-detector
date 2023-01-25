8e1eee4c4c466dbf85f4d1f885192f4aae141956
Max Goltzsche <max.goltzsche@gmail.com>
2023-01-25 04:08:59 +0100

    fix: fix restartPolicy, sleep infinity

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,4 +13,11 @@ if [ -d /host/etc ]; then
 fi
 
 # TODO: restart machine when config was changed.
-# TODO: make readinessprobe succeed only after this script completed successfully.
+# TODO: make readinessprobe succeed only at this point.
+
+if [ "$KEEP_RUNNING" = true ]; then
+	sleep infinity &
+	SPID=$!
+	trap "kill $SPID" 1 2 15
+	wait $SPID
+fi
