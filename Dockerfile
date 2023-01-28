255f47e200280d26575b1626bee5ecce80b7a72a
Max Goltzsche <max.goltzsche@gmail.com>
2023-01-28 01:47:34 +0100

    fix: reboot on config change, idempotent changes

--- Dockerfile
+++ Dockerfile
@@ -8,17 +8,20 @@ RUN make
 
 FROM debian:bookworm-slim
 ENV HIFIBERRYOS_VERSION=v20221128 \
-	KEEP_RUNNING=false
+	KEEP_RUNNING=false \
+	REBOOT_ON_CHANGE=false
 RUN apt-get update
 RUN DEBIAN_FRONTEND=noninteractive apt-get install -y bash alsa-utils sox i2c-tools wget
 RUN set -eux; \
-	wget -qO /bin/detect-hifiberry https://raw.githubusercontent.com/hifiberry/hifiberry-os/${HIFIBERRYOS_VERSION}/buildroot/package/hifiberry-tools/detect-hifiberry; \
-	chmod +x /bin/detect-hifiberry; \
 	mkdir -p /opt/hifiberry/bin; \
+	wget -qO /opt/hifiberry/bin/detect-hifiberry https://raw.githubusercontent.com/hifiberry/hifiberry-os/${HIFIBERRYOS_VERSION}/buildroot/package/hifiberry-tools/detect-hifiberry; \
+	chmod +x /opt/hifiberry/bin/detect-hifiberry; \
 	wget -qO /opt/hifiberry/bin/readhat https://raw.githubusercontent.com/hifiberry/hifiberry-os/${HIFIBERRYOS_VERSION}/buildroot/package/hifiberry-tools/readhat; \
 	chmod +x /opt/hifiberry/bin/readhat; \
+	wget -qO /opt/hifiberry/LICENSE https://raw.githubusercontent.com/hifiberry/hifiberry-os/${HIFIBERRYOS_VERSION}/LICENSE; \
 	printf '#!/bin/true\necho mount skipped' > /usr/local/bin/mount; \
-	chmod +x /usr/local/bin/mount
+	chmod +x /usr/local/bin/mount; \
+	ln -s /opt/hifiberry/bin/detect-hifiberry /bin/detect-hifiberry
 COPY --from=eepromutils /hats/eepromutils/eepdump /usr/bin/eepdump
 RUN eepdump --help
 COPY asound.conf /etc/asound.conf
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,23 +4,59 @@
 
 set -eu
 
-detect-hifiberry && (
-if [ -d /host/etc ]; then
-	if [ ! -f /host/etc/asound.conf ]; then
-		echo Installing /etc/asound.conf on the host
-		cp /etc/asound.conf /host/etc/asound.conf.tmp &&
-		mv /host/etc/asound.conf.tmp /host/etc/asound.conf
+backupBootConfig() {
+	if [ ! -f /boot/config.txt.bak ]; then
+		echo 'Creating /boot/config.txt backup at /boot/config.txt.bak'
+		cp -f /boot/config.txt /boot/config.txt.bak2
+		mv /boot/config.txt.bak2 /boot/config.txt.bak
+		sync
+	elif ! aplay -l | grep -q hifiberry | grep -q pcm5102; then
+		echo 'Restoring /boot/config.txt backup from /boot/config.txt.bak'
+		cp -f /boot/config.txt.bak /boot/config.txt2
+		mv /boot/config.txt2 /boot/config.txt
+		sync
 	fi
-fi
-) &&
-echo ready /tmp/ready &&
-echo Configuration is ready || echo 'ERROR: Hifiberry auto-configuration failed' >&2
+}
 
-# TODO: restart machine when config was changed.
+rebootIfBootConfigChanged() {
+	if [ "$REBOOT_ON_CHANGE" = true ]; then
+		if diff /boot/config.txt /tmp/boot-config.txt.prev; then
+			echo 'Rebooting the system since /boot/config.txt changed'
+			sleep 3
+			kill -2 1
+		fi
+	fi
+}
 
-if [ "$KEEP_RUNNING" = true ]; then
-	sleep infinity &
-	SPID=$!
-	trap "kill $SPID" 1 2 15
-	wait $SPID
-fi
+writeHostAlsaConfigIfNotExist() {
+	if [ -d /host/etc ]; then
+		if [ ! -f /host/etc/asound.conf ]; then
+			echo 'Installing /etc/asound.conf on the host'
+			cp -f /etc/asound.conf /host/etc/asound.conf.tmp &&
+			mv /host/etc/asound.conf.tmp /host/etc/asound.conf &&
+			sync
+		fi
+	fi
+}
+
+# ARGS: EXITCODE MESSAGE
+terminate() {
+	echo "$2"
+	if [ "$KEEP_RUNNING" = true ]; then
+		sleep infinity &
+		SPID=$!
+		trap "kill $SPID" 1 2 15
+		wait $SPID
+	else
+		exit $1
+	fi
+}
+
+
+cp -f /boot/config.txt /tmp/boot-config.txt.prev &&
+backupBootConfig && # Make detect-hifiberry idempotent, allow restoring config.
+detect-hifiberry && # Writes /boot/config.txt eventually, configuring dtoverlay.
+writeHostAlsaConfigIfNotExist &&
+echo ready > /tmp/ready &&
+rebootIfBootConfigChanged &&
+terminate 0 'Configuration is ready' || terminate 1 'ERROR: Hifiberry auto-configuration failed' >&2
